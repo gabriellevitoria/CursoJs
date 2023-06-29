db94bdb99d954cac2b0d3e332f022d77eef86a70
Gabrielle Vitória <gabriellev912@gmail.com>
2023-06-29 14:52:34 -0300

    CorreçãoAtividade

--- aula13/atividadeNumber/script.js
+++ aula13/atividadeNumber/script.js
@@ -1,32 +1,13 @@
-const numeroString = prompt('Digite um número'); // recebe o valor da variável
+const numero= Number(prompt('Digite um número')); //recebe o valor da variável
+const elementoResultado = document.getElementById('resultado');// acessa o elemento onde quer mostra-lá
+elementoResultado.innerHTML = numero;//atualizando o conteúdo 
 
-let numero = parseFloat(numeroString);
-        
 
-let elementoResultado = document.getElementById('resultado');// acessa o elemento onde quer mostra-lá
-elementoResultado.innerHTML = (numero)//atualizando o conteúdo 
-
-let raizQuadrada = numero ** (1/2);
-let elementoRaiz = document.getElementById('raizQuadrada');
-elementoRaiz.innerHTML = (`Raiz Quadrada = ${raizQuadrada}`);
-
-let inteiro = (Number.isInteger(numero)); 
-let elementoInteiro = document.getElementById('inteiro'); 
-elementoInteiro.innerHTML = (`O número <strong>${numero}</strong> é inteiro?  <strong>${inteiro}</strong>`);
-
-let not_a_number = isNaN(numero);
-let elemento_NaN = document.getElementById('NaN');
-elemento_NaN.innerHTML = (`É NaN: <strong>${not_a_number}</strong>`);
-        
-let arredondadoBaixo = Math.floor(numero);
-let elementoArredondarBaixo = document.getElementById('arredondadoBaixo');
-elementoArredondarBaixo.innerHTML = (`Arredondado para baixo: <strong>${arredondadoBaixo}</strong>`);
-
-let arredondadoCima = Math.ceil(numero);
-let elementoArredondarCima = document.getElementById('arredondadoCima');
-elementoArredondarCima.innerHTML = (`Arredondado para cima : <strong> ${arredondadoCima}</strong>`);
-
-let duasCasas = numero.toFixed(2); // ERRO
-let elementoDuasCasas = document.getElementById('duasCasas');
-elementoDuasCasas.innerHTML = (`Com duas casas decimais: <strong>${duasCasas}</strong>`)
-
+const texto = document.getElementById('texto');
+texto.innerHTML = 
+    `<p> A Raiz Quadrada do número <strong>${numero}</strong> é <strong>${numero ** (1/2)}</strong> </p>
+     <p>O número <strong>${numero}</strong> é inteiro?  <strong>${Number.isInteger(numero)}</strong> </p>
+     <p>É NaN: <strong>${Number.isNaN(numero)}</strong> </p>
+     <p>Arredondado para baixo: <strong>${Math.floor(numero)}</strong> </p>
+     <p>Arredondado para cima : <strong> ${Math.ceil(numero)}</strong> </p>
+     <p>Com duas casas decimais: <strong>${numero.toFixed(2)}</strong> </p>`;
