9950cfe1175f09ccc162658b5df4c6af7c3c7bfe
Gabrielle Vitória <gabriellev912@gmail.com>
2023-07-05 10:35:32 -0300

    Exercicio

--- Exercicios/FuncArrayObje/script.js
+++ Exercicios/FuncArrayObje/script.js
@@ -1,17 +1,44 @@
 function meuEscopo() {
       
     const form = document.querySelector('.form');
-    /*form.onsubmit = function(evento) {
-        evento.preventDefault ();
-        alert(1)
-        console.log('foi enviado')
-    };  */
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
 
      function recebeEventoForm (evento) {
-      const nome = abule;
+
+      evento.preventDefault();
+      const nome = form.querySelector('.nome');
+      const sobrenome  = form.querySelector('.sobrenome');
+      const peso = form.querySelector('.peso');
+      const altura = form.querySelector('.altura');
+
+      console.log(nome, sobrenome, peso, altura);
+
+      pessoas.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso : peso.value,
+        altura: altura.value
+
+      });
+
+      console.log(pessoas);
+
+      resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+      
     }
+
+
 
     form.addEventListener ('submit', recebeEventoForm);
 }
 
 meuEscopo();
+
+
+    /*form.onsubmit = function(evento) {
+        evento.preventDefault ();
+        alert(1)
+        console.log('foi enviado')
+    };  */
